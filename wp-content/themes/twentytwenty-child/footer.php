38321fd362ebaac59deb9c61bb351071b874edb1
flyplay91 <flyplay91@gmail.com>
2021-04-22 09:30:50 -0400

    added footer with acf

--- wp-content/themes/twentytwenty-child/footer.php
+++ wp-content/themes/twentytwenty-child/footer.php
@@ -11,49 +11,67 @@
  * @since Twenty Twenty 1.0
  */
 
+
+$email_address = get_field('email_address', 'option');
+$facebook_link = get_field('facebook_link', 'option');
+$instagram_link = get_field('instagram_link', 'option');
+$pinterest_link = get_field('pinterest_link', 'option');
+$description_text = get_field('description_text', 'option');
+$copyrighter_text = get_field('copyrighter_text', 'option');
+
 ?>
-			<footer id="site-footer" role="contentinfo" class="header-footer-group">
+			<footer class="site-footer">
+				<div class="footer-newsletter"></div>
 
-				<div class="section-inner">
+				<div class="footer-nav">
+					<div class="inner-section-1120 footer-nav__inner">
+						<div class="footer-contact">
+							<h2>Email Us</h2>
+							<?php if ($email_address) : ?>
+								<a href="mailto:<?php echo $email_address ?>"><?php echo $email_address ?></a>
+							<?php endif; ?>
+							<h2>Contact Us</h2>
+							<ul>
+								<?php if ($facebook_link) : ?>
+									<li><a href="<?php echo $facebook_link ?>"><i class="fab fa-facebook"></i></a></li>
+								<?php endif; ?>
+								<?php if ($instagram_link) : ?>
+									<li><a href="<?php echo $instagram_link ?>"><i class="fab fa-instagram"></i></a></li>
+								<?php endif; ?>
+								<?php if ($pinterest_link) : ?>
+									<li><a href="<?php echo $pinterest_link ?> "><i class="fab fa-pinterest"></i></a></li>
+								<?php endif; ?>
+							</ul>
+						</div>
+						<div class="footer-menu">
+							<?php if ( have_rows('menu_repeater', 'option')) :
+								while (have_rows('menu_repeater', 'option')) : the_row(); ?>
+								<div class="footer-menu--items">
+									<h2><?php echo get_sub_field('menu_title', 'option') ?></h2>
+									<ul>
+										<?php if ( have_rows('menu_item_repeater', 'option')) :
+											while (have_rows('menu_item_repeater', 'option')) : the_row(); ?>
+											<li><a href="<?php echo get_sub_field('menu_link') ?>"><?php echo get_sub_field('menu_text') ?></a></li>
+											<?php endwhile;
+										endif; ?>
+									</ul>
+								</div>
+								<?php endwhile;
+							endif; ?>
+						</div>
+					</div>
+				</div>
 
-					<div class="footer-credits">
-
-						<p class="footer-copyright">&copy;
-							<?php
-							echo date_i18n(
-								/* translators: Copyright date format, see https://www.php.net/manual/datetime.format.php */
-								_x( 'Y', 'copyright date format', 'twentytwenty' )
-							);
-							?>
-							<a href="<?php echo esc_url( home_url( '/' ) ); ?>"><?php bloginfo( 'name' ); ?></a>
-						</p><!-- .footer-copyright -->
-
-						<p class="powered-by-wordpress">
-							<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'twentytwenty' ) ); ?>">
-								<?php _e( 'Powered by WordPress', 'twentytwenty' ); ?>
-							</a>
-						</p><!-- .powered-by-wordpress -->
-
-					</div><!-- .footer-credits -->
-
-					<a class="to-the-top" href="#site-header">
-						<span class="to-the-top-long">
-							<?php
-							/* translators: %s: HTML character for up arrow. */
-							printf( __( 'To the top %s', 'twentytwenty' ), '<span class="arrow" aria-hidden="true">&uarr;</span>' );
-							?>
-						</span><!-- .to-the-top-long -->
-						<span class="to-the-top-short">
-							<?php
-							/* translators: %s: HTML character for up arrow. */
-							printf( __( 'Up %s', 'twentytwenty' ), '<span class="arrow" aria-hidden="true">&uarr;</span>' );
-							?>
-						</span><!-- .to-the-top-short -->
-					</a><!-- .to-the-top -->
-
-				</div><!-- .section-inner -->
-
-			</footer><!-- #site-footer -->
+				<div class="footer-copyright">
+					<div class="inner-section-1220 footer-copyright__inner">
+						<hr>
+						<div class="copyright-text">
+							<p><?php echo $description_text ?></p>
+							<p><?php echo $copyrighter_text ?></p>
+						</div>
+					</div>
+				</div>
+			</footer>
 
 		<?php wp_footer(); ?>
 
