84c5bbf3c7dbf03c7fefd40c6b243a1a688ffebe
flyplay91 <flyplay91@gmail.com>
2021-07-08 12:46:33 -0400

    fix blog post page

--- wp-content/themes/twentytwenty-child/templates/post.php
+++ wp-content/themes/twentytwenty-child/templates/post.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * Template Name: Blog Post Template
  * Template Post Type: post, page
@@ -18,10 +19,10 @@ get_header();
                 <h2>COLLECTIONS</h2>
                 <ul>
                     <?php
-                        $categories = get_categories();
-                        foreach($categories as $category) {
-                            echo '<li><a href="' . get_category_link($category->term_id) . '">' . $category->name . '</a></li>';
-                        }
+                    $categories = get_categories();
+                    foreach ($categories as $category) {
+                        echo '<li><a href="' . get_category_link($category->term_id) . '">' . $category->name . '</a></li>';
+                    }
                     ?>
                 </ul>
             </div>
@@ -29,37 +30,37 @@ get_header();
             <div class="post-left-bar--item">
                 <h2>Recommended</h2>
 
-                <?php if( have_rows('recommendation_group')) :
+                <?php if (have_rows('recommendation_group')) :
                     $recomendation_group = get_field('recommendation_group');
-                    while ( have_rows('recommendation_group')): the_row(); ?>
-                    <div class="recommended-posts">
-                        <?php if( have_rows('post_repeater') ) :
-                            while( have_rows('post_repeater') ) : the_row();
-                            $post_item = get_sub_field('post_item');
-                            $post_id = $post_item->ID;
-                            $post_title = get_the_title($post_id);
-                            $post_link = get_post_permalink($post_id);
-                            $categories = get_the_category($post_id);
-                            
+                    while (have_rows('recommendation_group')) : the_row(); ?>
+                        <div class="recommended-posts">
+                            <?php if (have_rows('post_repeater')) :
+                                while (have_rows('post_repeater')) : the_row();
+                                    $post_item = get_sub_field('post_item');
+                                    $post_id = $post_item->ID;
+                                    $post_title = get_the_title($post_id);
+                                    $post_link = get_post_permalink($post_id);
+                                    $categories = get_the_category($post_id);
+
                             ?>
-                                <div class="recommended-post">
-                                    <div class="widget-post-thumb">
-                                        <a href="<?php echo $post_link; ?>">
-                                            <img src="<?php echo wp_get_attachment_url( get_post_thumbnail_id( $post_id )) ?>">
-                                        </a>
-                                    </div>
-                                    <div class="widget-post-description">
-                                        <div class="widget-blog-title">
+                                    <div class="recommended-post">
+                                        <div class="widget-post-thumb">
                                             <a href="<?php echo $post_link; ?>">
-                                                <?php echo $post_title; ?>
+                                                <img src="<?php echo wp_get_attachment_url(get_post_thumbnail_id($post_id)) ?>">
                                             </a>
                                         </div>
+                                        <div class="widget-post-description">
+                                            <div class="widget-blog-title">
+                                                <a href="<?php echo $post_link; ?>">
+                                                    <?php echo $post_title; ?>
+                                                </a>
+                                            </div>
+                                        </div>
                                     </div>
-                                </div>
                             <?php endwhile;
-                        endif; ?>
-                    </div>
-                    <?php endwhile;
+                            endif; ?>
+                        </div>
+                <?php endwhile;
                 endif; ?>
             </div>
         </div>
@@ -67,13 +68,25 @@ get_header();
         <div class="post-content">
             <div class="post-breadcrumb"></div>
             <div class="post-content__inner">
-                <h1></h1>
-                <span class="post-author"></span>
-                <span class="post-date"></span>
+                <h1><?php the_title(); ?></h1>
+                <span class="post-author">
+                    <?php
+                    $author_id = $post->post_author;
+                    the_author_meta('user_nicename', $author_id);
+                    ?>
+                </span>
+                <span class="post-date">
+                    <?php echo get_the_date(); ?>
+                </span>
+                <div class="post-image">
+                    <?php echo the_post_thumbnail(); ?>
+                </div>
 
-                <?php
+                <div class="post-content">
+                    <?php
                     the_content();
-                ?>
+                    ?>
+                </div>
             </div>
         </div>
 
